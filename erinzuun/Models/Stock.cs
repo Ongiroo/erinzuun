064828eb10118509491ad765edd74ac864ac6f4d
Amgalanbaatar Baatarjav <amgalan.amg@gmail.com>
2018-09-03 21:58:05 +0100

    Update Stock.cs

--- erinzuun/Models/Stock.cs
+++ erinzuun/Models/Stock.cs
@@ -9,9 +9,14 @@ namespace erinzuun.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public string Tick { get; set; }
         public DateTime ReleaseDate { get; set; }
         public DateTime DevelopmentDate { get; set; }
         public string Type { get; set; }
+        public decimal AbsoluteReturn { get; set; }
+        public decimal ExpectedReturn { get; set; }
+        public string Bid { get; private set; }
+        public string Ask { get; private set; }
         public decimal Price { get; set; }
     }
 }
